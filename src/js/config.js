0791ff4f5667b580bb0fb93d4135ead3bb13e38d
santosh_motapandle <santoshmotapandle@gmail.com>
2025-06-28 23:59:37 +0530

    Iniial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6ab61e30-3cf6-419a-a239-4492e4673184';
+export const MODAL_CLOSE_SEC = 2.5;
